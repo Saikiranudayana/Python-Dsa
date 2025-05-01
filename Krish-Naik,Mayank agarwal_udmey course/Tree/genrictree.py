76512c5a6b42c11ba7484dac150809c7f934068b
saikiranudayana <saikiranudayana@gmail.com>
2025-05-01 12:09:45 +0530

    pre order and post order

--- Krish-Naik,Mayank agarwal_udmey course/Tree/genrictree.py
+++ Krish-Naik,Mayank agarwal_udmey course/Tree/genrictree.py
@@ -27,7 +27,30 @@ def predefineed_genric_tree_inputs():
     
     
     return root1,root2
+
+
+def preorder_traversl(root):
+    if root is None:
+        return 
+    print(root.dat,end="")
+    for eachchild in root.children:
+        preorder_traversl(eachchild)
+        
+def post_order(root):
+    if root is None:
+        return 
     
-    
-    
-    
+    for eachchild in root.children:
+        post_order(eachchild)
+        
+    print(root.data,end=" ")
+        
+
+
+root1,root2 = predefineed_genric_tree_inputs()
+print(preorder_traversl())
+
+
+
+root1,root2 = predefineed_genric_tree_inputs()
+print(post_order())
